4fe2d9066ea589b4b43f67d3e90484f0fa38aa9a
AlexandrBL <bbelozyrov.aaleksandr@gmail.com>
2024-02-07 13:34:33 +0300

    clear skeleton

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,17 +1,4 @@
 const router = require('express').Router();
-const productsRouter = require('./view/products.route');
-const errRouter = require('./view/err.route');
-const productsApiRouter = require('./api/products.api.route');
-const authRouter = require('./view/auth.route');
-const authApiRouter = require('./api/auth.api.route');
-const { ifNotAuthRedirect } = require('../middleware/auth');
 
-router.use('/api/auth', authApiRouter);
-router.use('/api/products', productsApiRouter);
-
-router.use('/auth', authRouter);
-router.use('/products', ifNotAuthRedirect, productsRouter);
-
-router.use('*', ifNotAuthRedirect, errRouter);
 
 module.exports = router;
