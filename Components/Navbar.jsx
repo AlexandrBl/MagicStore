95117c3883962ad8413c27cfba1e26fe48987168
Ayur67 <aur67@mail.ru>
2024-02-07 14:31:38 +0300

    nav classNames done

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,28 +2,28 @@ const React = require("react");
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__el">
           <a href="">Главная</a>
         </li>
-        <li>
+        <li className="nav__el">
           <a href="">Личный кабинет</a>
         </li>
-        <li>
+        <li className="nav__el">
           <a href="">Корзина</a>
         </li>
-        <li>
+        <li className="nav__el">
           <a href="">Товары</a>
         </li>
-        <li>
+        <li className="nav__el">
           <a href="">loga</a>
         </li>
-        <li>
+        <li className="nav__el">
           <a href="">Выход</a>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
