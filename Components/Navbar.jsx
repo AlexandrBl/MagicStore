a0ade071262b8aa0c7797396793fb0c810cc9bed
Ayur67 <aur67@mail.ru>
2024-02-07 13:56:04 +0300

    Navbar done

---
+++ Components/Navbar.jsx
@@ -0,0 +1,17 @@
+const React = require("react");
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="">Главная</a>
+      <a href="">Личный кабинет</a>
+      <a href="">Корзина</a>
+      <a href="">Товары</a>
+      <a href="">loga</a>
+      <a href="">rega</a>
+      <a href="">Выход</a>
+    </div>
+  );
+}
+
+module.exports = Navbar;
