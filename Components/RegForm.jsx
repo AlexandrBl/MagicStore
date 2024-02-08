8747f913f7b8d1761db12685aa802624007c5da7
AlexandrBL <bbelozyrov.aaleksandr@gmail.com>
2024-02-08 15:33:49 +0300

    fix select from cities

--- Components/RegForm.jsx
+++ Components/RegForm.jsx
@@ -10,7 +10,7 @@ function RegForm({ cities }) {
       <input type="text" name="email" className="reg-form__input input" />
       <input className="reg-form__input input" name="city" list="cities" />
       <datalist className="reg-form__datalist" id="cities">
-        {cities.map((el) => <option className="reg-form__option">{el.id}</option>)}
+        {cities.map((el) => <option className="reg-form__option">{el.name}</option>)}
       </datalist>
       <input type="text" name="password" className="reg-form__input input" />
       <input type="text" name="password2" className="reg-form__input input" />
