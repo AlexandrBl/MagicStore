53393c6720799c714f94fb079534ee2f54c12769
AlexandrBL <bbelozyrov.aaleksandr@gmail.com>
2024-02-09 10:52:14 +0300

    fix require

--- routes/view/basket.route.js
+++ routes/view/basket.route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const BasketPage = require('../../Components/BasketPage');
-const Product = require('../../db/models');
+const {Product, OrderItem} = require('../../db/models');
 
 router.get('/', async (req, res) => {
   try {
     const { idUser } = res.locals.user.id;
-    const products = await Product.findAll({ where: { user_id: idUser } });
+    const products = await OrderItem.findAll({ where: { user_id: idUser } });
     const html = res.renderComponent(BasketPage, {
       title: 'Basket',
       products,
